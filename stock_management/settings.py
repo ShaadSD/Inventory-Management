e1395a38df50b4a7f3be29bd0078bf19ed93c7c2
Altuhim Md. Shaad <144701936+ShaadSD@users.noreply.github.com>
2025-10-27 19:43:46 +0600

    add csrf

--- stock_management/settings.py
+++ stock_management/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = 'django-insecure-@m5t=l68h8%rr=uoy7o5b7moqoimo#1wxk178olg)u#*h25iry
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+CSRF_TRUSTED_ORIGINS = ['http://inventory-management-9hzm.onrender.com','https://*.127.0.0.1']
 
 
 # Application definition
